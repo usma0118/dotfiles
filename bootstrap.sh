e89c5c5d22884ffc58adc9ea39be7063a6704d0a
usma0118 <usmanmasood@msn.com>
2024-05-14 14:02:36 +0200

    fixed minor issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ $(uname) == 'Linux' ]]; then
     apt-add-repository ppa:ansible/ansible
-    apt-get update
-    apt-get install ansible software-properties-common git
+    apt-get update -y
+    apt-get install ansible software-properties-common git -y
 elif [[ $(uname) == 'Darwin' ]]; then
     xcode-select --install
     brew install ansible
