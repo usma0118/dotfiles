76d10e639ab230125b3c1f7898bbe71b342ab60c
usma0118 <usmanmasood@msn.com>
2025-09-05 12:13:32 +0000

    using ansible-core

--- bootstrap.sh
+++ bootstrap.sh
@@ -64,7 +64,7 @@ if ! have ansible || ! have ansible-playbook ; then
       else
         log_info "Homebrew already installed"
       fi
-      brew install ansible git direnv
+      brew install ansible-core git
       ;;
     *)
       abort "Unsupported OS family: ${os_family}"
